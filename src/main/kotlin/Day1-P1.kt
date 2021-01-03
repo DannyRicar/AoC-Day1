331015734fbfb9d4cc4bc629b9768c0ee144c66a
DannyRicar <43332318+DannyRicar@users.noreply.github.com>
2021-01-03 23:54:52 +0100

    Initial commit

---
+++ src/main/kotlin/Day1-P1.kt
@@ -0,0 +1,39 @@
+class Day1 {
+    fun solve(inputStrs: List<String?>?) {
+        val inputs: List<Int> = convertToInts(inputStrs)
+        var complete = false
+        for (i in inputs.indices) {
+            if (complete) break
+            for (j in i + 1 until inputs.size) {
+                val int1 = inputs[i]
+                val int2 = inputs[j]
+                if (int1 + int2 == 2020) {
+                    lap(int1 * int2)
+                    complete = true
+                    break
+                }
+            }
+        }
+        for (i in inputs.indices) {
+            for (j in i + 1 until inputs.size) {
+                for (k in j + 1 until inputs.size) {
+                    val int1 = inputs[i]
+                    val int2 = inputs[j]
+                    val int3 = inputs[k]
+                    if (int1 + int2 + int3 == 2020) {
+                        lap(int1 * int2 * int3)
+                        return
+                    }
+                }
+            }
+        }
+    }
+
+    private fun lap(i: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun convertToInts(inputStrs: List<String?>?): List<Int> {
+
+    }
+}
